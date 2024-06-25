e3d3f944ea7ec81cf5c138e9fafeb9a32a20afe2
BubbleFish <daforsastudia@gmail.com>
2024-06-25 18:15:43 +0300

    feat(backend): Add CORS support for all origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from "./app.module"
 async function bootstrap() {
     require("dotenv").config()
     const app = await NestFactory.create(AppModule)
+    app.enableCors({
+        origin: "*",
+        methods: ["GET"],
+    })
     await app.listen(3000)
 }
 bootstrap()
