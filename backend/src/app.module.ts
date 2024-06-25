cbc347366c238df597ea2f4b5e0681dd99648f44
BubbleFish <daforsastudia@gmail.com>
2024-06-25 12:32:56 +0300

    refactor: Configure Prettier & format code

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ApiController } from './api/api.controller';
+import { Module } from "@nestjs/common"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+import { ApiController } from "./api/api.controller"
+import { HttpModule } from "@nestjs/axios"
+import { ApiService } from "./api/api.service"
 
 @Module({
-  imports: [],
-  controllers: [AppController, ApiController],
-  providers: [AppService],
+    imports: [HttpModule],
+    controllers: [AppController, ApiController],
+    providers: [AppService, ApiService],
 })
 export class AppModule {}
